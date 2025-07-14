35b830483d3ac22d1d5c91d28644f1b53180553c
Efrain <efrain.csm@gmail.com>
2025-07-14 23:24:08 +0200

    feat(user-customer): add endpoints

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,6 +3,14 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Shop\GetAllProductCategoriesController;
+use App\Http\Controllers\Shop\LoginController;
+use App\Http\Controllers\Shop\LogoutController;
+use App\Http\Controllers\Shop\RegisterUserWithCustomerController;
+
+// Authentication
+Route::post('/auth/register', RegisterUserWithCustomerController::class);
+Route::post('/auth/login', LoginController::class)->middleware('throttle.login:5,1');
+Route::post('/auth/logout', LogoutController::class)->middleware('auth:sanctum');
 
 // Categories and Products
 Route::get('/categories', GetAllProductCategoriesController::class);
