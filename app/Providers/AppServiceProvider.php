6771efb3d3e78e96a62aee80db25281744775a09
Efrain <efrain.csm@gmail.com>
2025-07-14 17:27:17 +0200

    feat(app): override default factory resolver logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Make it look for the Factory in the default directory "Database\Factories"
+        // instead of "Database\Factories\Src\Shop\****\Infrastructure\Persistence\Eloquent\****Factory".
+        Factory::guessFactoryNamesUsing(function (string $model_name) {
+            $namespace = 'Database\\Factories\\';
+            $model_name = Str::remove('EloquentModel', Str::afterLast($model_name, '\\'));
+            return $namespace . $model_name . 'Factory';
+        });
     }
 }
