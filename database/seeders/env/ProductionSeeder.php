c9b0dcb7e1cbee4ce19f72ef2ad688eeaeb0695b
Efrain <efrain.csm@gmail.com>
2025-07-14 20:53:55 +0200

    feat(product-category): seeders required

--- database/seeders/env/ProductionSeeder.php
+++ database/seeders/env/ProductionSeeder.php
@@ -4,11 +4,40 @@ declare(strict_types=1);
 
 namespace Database\Seeders\Env;
 
+use Database\Seeders\Helpers\ProductCategorySeederHelper;
 use Illuminate\Database\Seeder;
 
 class ProductionSeeder extends Seeder
 {
+    public function __construct(
+        private ProductCategorySeederHelper $productCategorySeederHelper,
+    ) {
+    }
+
     public function run(): void
     {
+        // GENERATE
+
+        $productCategoriesData = [
+            [
+                'id' => '019773a5-3441-7fb8-8f7c-8ba2d103ce06',
+                'name' => 'Man',
+                'slug' => 'man',
+            ],
+            [
+                'id' => '019773a5-3442-7fee-a6a9-b9d1896e44d0',
+                'name' => 'Woman',
+                'slug' => 'woman',
+            ],
+            [
+                'id' => '019773a5-3443-7bfd-b958-8348ff4b223d',
+                'name' => 'Child',
+                'slug' => 'child',
+            ],
+        ];
+
+        // PERSIST
+
+        $this->productCategorySeederHelper->persistList($productCategoriesData);
     }
 }
