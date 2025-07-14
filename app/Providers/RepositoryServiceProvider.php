2f347909fb37dd90b3fd0acbba04ca21bf951097
Efrain <efrain.csm@gmail.com>
2025-07-14 23:19:50 +0200

    feat(user-customer): update providers

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Src\Shop\Customer\Domain\CustomerRepositoryInterface;
+use Src\Shop\Customer\Infrastructure\Repository\EloquentCustomerRepository;
 use Src\Shop\ProductCategory\Domain\ProductCategoryRepositoryInterface;
 use Src\Shop\ProductCategory\Infrastructure\Repository\EloquentProductCategoryRepository;
+use Src\Shop\User\Domain\UserRepositoryInterface;
+use Src\Shop\User\Infrastructure\Repository\EloquentUserRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->app->bind(CustomerRepositoryInterface::class, EloquentCustomerRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, EloquentUserRepository::class);
         $this->app->bind(ProductCategoryRepositoryInterface::class, EloquentProductCategoryRepository::class);
     }
 
