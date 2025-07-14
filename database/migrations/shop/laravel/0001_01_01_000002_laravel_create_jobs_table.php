e9b0981e3affaea8b6b49855fdef0530027bcbe0
Efrain <efrain.csm@gmail.com>
2025-07-14 20:49:50 +0200

    refactor(migrations): reorganize migrations

--- database/migrations/shop/laravel/0001_01_01_000002_laravel_create_jobs_table.php
+++ database/migrations/shop/laravel/0001_01_01_000002_laravel_create_jobs_table.php
@@ -12,7 +12,7 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::connection('mysql-laravel')->create('jobs', function (Blueprint $table) {
             $table->id();
             $table->string('queue')->index();
             $table->longText('payload');
@@ -22,7 +22,7 @@ return new class () extends Migration {
             $table->unsignedInteger('created_at');
         });
 
-        Schema::create('job_batches', function (Blueprint $table) {
+        Schema::connection('mysql-laravel')->create('job_batches', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->string('name');
             $table->integer('total_jobs');
@@ -35,7 +35,7 @@ return new class () extends Migration {
             $table->integer('finished_at')->nullable();
         });
 
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::connection('mysql-laravel')->create('failed_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->unique();
             $table->text('connection');
@@ -51,8 +51,8 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('jobs');
-        Schema::dropIfExists('job_batches');
-        Schema::dropIfExists('failed_jobs');
+        Schema::connection('mysql-laravel')->dropIfExists('jobs');
+        Schema::connection('mysql-laravel')->dropIfExists('job_batches');
+        Schema::connection('mysql-laravel')->dropIfExists('failed_jobs');
     }
 };
