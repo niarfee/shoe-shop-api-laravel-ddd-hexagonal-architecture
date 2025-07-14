fe17e89d10824fc8ee741bfb665bda701833d654
Efrain <efrain.csm@gmail.com>
2025-07-14 21:40:29 +0200

    feat(product-category): add endpoint get all

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,3 +1,8 @@
 <?php
 
 declare(strict_types=1);
+
+use App\Http\Controllers\Shop\GetAllProductCategoriesController;
+
+// Categories and Products
+Route::get('/categories', GetAllProductCategoriesController::class);
