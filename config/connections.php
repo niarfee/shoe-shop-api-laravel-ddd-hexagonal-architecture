08ada74367bd1618f3c12fad6fc5e471b7a02f9f
Efrain <efrain.csm@gmail.com>
2025-07-14 12:39:18 +0200

    chore(env): add environment setup with Docker

---
+++ config/connections.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'mysql_laravel_connection' => env('DB_MYSQL_LARAVEL_CONNECTION', 'mysql-laravel'),
+    'mysql_shop_connection' => env('DB_MYSQL_SHOP_CONNECTION', 'mysql-shop'),
+];
