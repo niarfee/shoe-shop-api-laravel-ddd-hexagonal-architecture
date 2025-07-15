3f911514bcd18fe68d674f432106f2971261cb70
Efrain <efrain.csm@gmail.com>
2025-07-15 13:15:02 +0200

    feat(product): update provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -7,6 +7,10 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Src\Shop\Customer\Domain\CustomerRepositoryInterface;
 use Src\Shop\Customer\Infrastructure\Repository\EloquentCustomerRepository;
+use Src\Shop\Product\Domain\ProductRepositoryInterface;
+use Src\Shop\Product\Domain\ProductVariantRepositoryInterface;
+use Src\Shop\Product\Infrastructure\Repository\EloquentProductRepository;
+use Src\Shop\Product\Infrastructure\Repository\EloquentProductVariantRepository;
 use Src\Shop\ProductCategory\Domain\ProductCategoryRepositoryInterface;
 use Src\Shop\ProductCategory\Infrastructure\Repository\EloquentProductCategoryRepository;
 use Src\Shop\User\Domain\UserRepositoryInterface;
@@ -19,6 +23,8 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(CustomerRepositoryInterface::class, EloquentCustomerRepository::class);
         $this->app->bind(UserRepositoryInterface::class, EloquentUserRepository::class);
         $this->app->bind(ProductCategoryRepositoryInterface::class, EloquentProductCategoryRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, EloquentProductRepository::class);
+        $this->app->bind(ProductVariantRepositoryInterface::class, EloquentProductVariantRepository::class);
     }
 
     public function boot(): void
