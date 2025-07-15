fe08ee834e5cb530682634380fc10bc88cd13507
Efrain <efrain.csm@gmail.com>
2025-07-15 14:55:56 +0200

    feat(order): add endpoints

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,12 +2,17 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\Shop\ConfirmCartController;
 use App\Http\Controllers\Shop\GetAllProductCategoriesController;
+use App\Http\Controllers\Shop\GetCartController;
+use App\Http\Controllers\Shop\GetOrdersController;
 use App\Http\Controllers\Shop\GetProductController;
 use App\Http\Controllers\Shop\GetProductsByProductCategoryController;
 use App\Http\Controllers\Shop\LoginController;
 use App\Http\Controllers\Shop\LogoutController;
 use App\Http\Controllers\Shop\RegisterUserWithCustomerController;
+use App\Http\Controllers\Shop\UpdateCartController;
+use Illuminate\Support\Facades\Route;
 
 // Authentication
 Route::post('/auth/register', RegisterUserWithCustomerController::class);
@@ -18,3 +23,11 @@ Route::post('/auth/logout', LogoutController::class)->middleware('auth:sanctum')
 Route::get('/categories', GetAllProductCategoriesController::class);
 Route::get('/categories/{id}/products', GetProductsByProductCategoryController::class);
 Route::get('/products/{id}', GetProductController::class);
+
+// Cart
+Route::get('/cart', GetCartController::class)->middleware('auth:sanctum');
+Route::put('/cart/update', UpdateCartController::class)->middleware('auth:sanctum');
+Route::post('/cart/confirm', ConfirmCartController::class)->middleware('auth:sanctum');
+
+// Orders
+Route::get('/orders', GetOrdersController::class)->middleware('auth:sanctum');
