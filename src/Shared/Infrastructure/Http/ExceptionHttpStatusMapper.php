c9fd1b49f2b0a64f442d7feabaf464c1dae231f7
Efrain <efrain.csm@gmail.com>
2025-07-15 13:45:57 +0200

    feat(product): update ExceptionHttpStatusMapper

--- src/Shared/Infrastructure/Http/ExceptionHttpStatusMapper.php
+++ src/Shared/Infrastructure/Http/ExceptionHttpStatusMapper.php
@@ -9,14 +9,18 @@ use Illuminate\Validation\ValidationException;
 use Src\Shared\Domain\Exception\EmptyPasswordException;
 use Src\Shared\Domain\Exception\InvalidEmailDomainException;
 use Src\Shared\Domain\Exception\InvalidEmailFormatException;
+use Src\Shared\Domain\Exception\InvalidPriceException;
 use Src\Shared\Domain\Exception\InvalidStringLengthException;
 use Src\Shared\Domain\Exception\InvalidTokenStringException;
 use Src\Shared\Domain\Exception\InvalidTokenTypeException;
 use Src\Shared\Domain\Exception\InvalidUuidException;
 use Src\Shared\Domain\Exception\PasswordConfirmationDoesNotMatchException;
 use Src\Shop\Customer\Domain\Exception\DuplicateCustomerEmailException;
+use Src\Shop\Product\Domain\Exception\ProductNotFoundException;
 use Src\Shop\ProductCategory\Domain\Exception\InvalidProductCategorySlugException;
 use Src\Shop\ProductCategory\Domain\Exception\NoProductCategoriesExistException;
+use Src\Shop\ProductCategory\Domain\Exception\ProductCategoryNotFoundException;
+use Src\Shop\Shared\Domain\Exception\ProductVariantStockNegativeException;
 use Src\Shop\User\Domain\Exception\InvalidCredentialsException;
 use Throwable;
 
@@ -38,6 +42,10 @@ final class ExceptionHttpStatusMapper
             $e instanceof InvalidTokenTypeException => HttpStatusEnum::Unauthorized,
             $e instanceof InvalidEmailFormatException => HttpStatusEnum::UnprocessableEntity,
             $e instanceof InvalidTokenStringException => HttpStatusEnum::Unauthorized,
+            $e instanceof InvalidPriceException => HttpStatusEnum::NotFound,
+            $e instanceof ProductNotFoundException => HttpStatusEnum::NotFound,
+            $e instanceof ProductCategoryNotFoundException => HttpStatusEnum::NotFound,
+            $e instanceof ProductVariantStockNegativeException => HttpStatusEnum::UnprocessableEntity,
 
             // Laravel exceptions
             $e instanceof AuthenticationException => HttpStatusEnum::Unauthorized,
