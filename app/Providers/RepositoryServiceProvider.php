5fdd7a63e416a4bf4c5537480931e412f6872763
Efrain <efrain.csm@gmail.com>
2025-07-15 15:01:12 +0200

    feat(order): update provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -7,6 +7,10 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Src\Shop\Customer\Domain\CustomerRepositoryInterface;
 use Src\Shop\Customer\Infrastructure\Repository\EloquentCustomerRepository;
+use Src\Shop\Order\Domain\OrderLineRepositoryInterface;
+use Src\Shop\Order\Domain\OrderRepositoryInterface;
+use Src\Shop\Order\Infrastructure\Repository\EloquentOrderLineRepository;
+use Src\Shop\Order\Infrastructure\Repository\EloquentOrderRepository;
 use Src\Shop\Product\Domain\ProductRepositoryInterface;
 use Src\Shop\Product\Domain\ProductVariantRepositoryInterface;
 use Src\Shop\Product\Infrastructure\Repository\EloquentProductRepository;
@@ -25,6 +29,8 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(ProductCategoryRepositoryInterface::class, EloquentProductCategoryRepository::class);
         $this->app->bind(ProductRepositoryInterface::class, EloquentProductRepository::class);
         $this->app->bind(ProductVariantRepositoryInterface::class, EloquentProductVariantRepository::class);
+        $this->app->bind(OrderRepositoryInterface::class, EloquentOrderRepository::class);
+        $this->app->bind(OrderLineRepositoryInterface::class, EloquentOrderLineRepository::class);
     }
 
     public function boot(): void
